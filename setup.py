b4c398fa3bf2aa47a780555b5742e406c544827e
Thomas Ballinger <thomasballinger@gmail.com>
2013-07-18 22:32:57 -0400

    make package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='strfmt',
+      version='0.0.1',
+      description='string-like objects marked up with terminal formatting',
+      url='https://github.com/thomasballinger/fmtstr',
+      author='Thomas Ballinger',
+      author_email='thomasballinger@gmail.com',
+      license='MIT',
+      packages=['fmtstr'],
+      zip_safe=False)
