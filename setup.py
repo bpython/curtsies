fa843e5799c71a987fef84f99a2e60bfcbec7648
Thomas Ballinger <thomasballinger@gmail.com>
2013-08-18 01:54:24 -0400

    schmancy package info

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ setup(name='strfmt',
       author_email='thomasballinger@gmail.com',
       license='MIT',
       packages=['fmtstr'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
       zip_safe=False)
