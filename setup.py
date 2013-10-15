b3d2124f560689b905b83c438e76cffada9efeda
Thomas Ballinger <thomasballinger@gmail.com>
2013-10-15 19:36:08 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fmtstr',
-      version='0.0.10',
+      version='0.0.11',
       description='string-like objects marked up with terminal formatting',
       url='https://github.com/thomasballinger/fmtstr',
       author='Thomas Ballinger',
