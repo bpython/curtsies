9522291b2bb92b801849d7e508f16e8e498e051e
Thomas Ballinger <thomasballinger@gmail.com>
2014-06-20 12:17:03 -0400

    timeout using select

--- curtsies/input.py
+++ curtsies/input.py
@@ -5,6 +5,7 @@ New terminal
 import fcntl
 import os
 import signal
+import select
 import sys
 import termios
 import time
@@ -23,6 +24,25 @@ class nonblocking(object):
         fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
     def __exit__(self, *args):
         fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl)
+
+class timeout(object):
+    def __init__(self, stream, timeout):
+        """Set stream to timeout after timeout tenths of a second"""
+        self.stream = stream
+        self.fd = self.stream.fileno()
+        if timeout < 0 or timeout > 255:
+            raise ValueError("Timeout must be from 0 to 255 inclusive")
+        self.timeout = timeout
+
+    def __enter__(self):
+        self.orig_term = termios.tcgetattr(self.fd)
+        new_term = self.orig_term[:]
+        new_term[3] &= ~termios.ICANON
+        new_term[6][termios.VTIME] = self.timeout
+        termios.tcsetattr(self.fd, termios.TCSAFLUSH, new_term)
+
+    def __exit__(self, *args):
+        termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.orig_term)
 
 class Input(object):
     def __init__(self, in_stream=sys.stdin):
@@ -45,9 +65,6 @@ class Input(object):
 
     def send(self, timeout=None):
         """Returns a key or None if no key pressed"""
-        if timeout is None:
-            timeout = 10**10 # 300 years
-        #TODO block on read instead (can read take a timeout?)
 
         def find_key():
             """Returns the keypress identified by adding unprocessed bytes, or None"""
@@ -66,19 +83,20 @@ class Input(object):
         if e is not None:
             return e
 
-        if self.nonblocking_read():
-            e = find_key()
-            assert e is not None
-            return e
+        #if self.nonblocking_read():
+        #    e = find_key()
+        #    assert e is not None
+        #    return e
+        # I've prev seen some problems with select - would checking the above first help?
 
-        #TODO use a stdin read with a timeout: http://stackoverflow.com/a/2918103/398212
-        t0 = time.time()
-        while time.time() - t0 < timeout:
-            if self.nonblocking_read():
-                e = find_key()
-                assert e is not None
-                return e
-        return None
+        #TODO use a stdin read with a timeout instead?: http://stackoverflow.com/a/2918103/398212
+        (rs, _, _) = select.select([self.in_stream.fileno()], [], [], timeout)
+        if not rs:
+            return None
+        assert self.nonblocking_read()
+        e = find_key()
+        assert e is not None
+        return e
 
     def nonblocking_read(self):
         """returns False if no waiting character, else True and adds it to self.unprocessed_bytes"""
@@ -112,8 +130,11 @@ def main():
         for e in input_generator:
             print repr(e)
 
+def testfunc():
+    with timeout(sys.stdin, 10):
+        print repr(os.read(sys.stdin.fileno(), 1024))
+
+
 if __name__ == '__main__':
     main()
     #testfunc()
-
-
