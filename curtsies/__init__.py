863f28ffd454e3ebff01fbc0c17d531f2c32060a
Thomas Ballinger <thomasballinger@gmail.com>
2014-07-02 14:59:27 -0400

    Factor out Termmode for arbitrary tcattrs

--- curtsies/__init__.py
+++ curtsies/__init__.py
@@ -3,7 +3,7 @@ __version__='0.1.0'
 
 from .window import FullscreenWindow, CursorAwareWindow
 from .input import Input
-from .termhelpers import Nonblocking, Cbreak
+from .termhelpers import Nonblocking, Cbreak, Termmode
 from .formatstring import FmtStr, fmtstr
 from .formatstringarray import FSArray, fsarray
 
