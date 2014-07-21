0f2e6dbd593f45a53e54e6cf7828e7940a2535b8
Thomas Ballinger <thomasballinger@gmail.com>
2014-07-21 09:55:44 -0400

    reproducing bytes/chars bug

---
+++ examples/initial_input_with_cursor.py
@@ -0,0 +1,19 @@
+import time
+
+from curtsies import *
+
+
+
+def main():
+    """Ideally we shouldn't lose the first second of events"""
+    with Input() as input_generator:
+        def extra_bytes_callback(string):
+            print('got extra bytes', repr(string))
+            input_generator.unget_bytes(string)
+        time.sleep(1)
+        with CursorAwareWindow(extra_bytes_callback=extra_bytes_callback) as window:
+            window.get_cursor_position()
+            for e in input_generator:
+                print(repr(e))
+if __name__ == '__main__':
+    main()
