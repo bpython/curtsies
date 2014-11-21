bcfac2d1af8a41017306a4fcc59b61e8a9e095f6
Thomas Ballinger <thomasballinger@gmail.com>
2014-11-21 11:23:10 -0500

    better realtime example

--- examples/fps.py
+++ examples/fps.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 
 import time
 
@@ -6,38 +6,38 @@ from curtsies import FullscreenWindow, Input, FSArray
 from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, on_red
 import curtsies.events
 
-FPS = 15
-
 class Frame(curtsies.events.ScheduledEvent):
     pass
-
-reactor = Input()
-schedule_next_frame = reactor.scheduled_event_trigger(Frame)
-schedule_next_frame(when=time.time())
-dt = float(1)/FPS
-events_since_frame = []
 
 class World(object):
     def __init__(self):
         self.s = 'Hello'
-    def tick(self, events):
-        self.s += '|' + ''.join(str(x) for x in events)
+    def tick(self):
+        self.s += '|'
         self.s = self.s[max(1, len(self.s)-80):]
+    def process_event(self, e):
+        self.s += str(e)
 
-world = World()
+def realtime(fps=15):
+    world = World()
+    dt = 1/fps
 
-with reactor:
-    for e in reactor:
-        if isinstance(e, Frame):
-            world.tick(events_since_frame)
-            del events_since_frame[:]
-            print(world.s)
-            when = e.when + dt
-            while when < time.time():
-                when += dt
-            schedule_next_frame(when)
-        elif e == u'<ESC>':
-            break
-        else:
-            events_since_frame.append(e)
+    reactor = Input()
+    schedule_next_frame = reactor.scheduled_event_trigger(Frame)
+    schedule_next_frame(when=time.time())
 
+    with reactor:
+        for e in reactor:
+            if isinstance(e, Frame):
+                world.tick()
+                print(world.s)
+                when = e.when + dt
+                while when < time.time():
+                    when += dt
+                schedule_next_frame(when)
+            elif e == u'<ESC>':
+                break
+            else:
+                world.process_event(e)
+
+realtime()
