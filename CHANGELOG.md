91bf877ee4a65664a79845ece7794ba3eb4209ea
Thomas Ballinger <thomasballinger@gmail.com>
2016-07-29 15:25:14 -0400

    release 0.2.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
 
 ## [Unreleased]
+
+## [0.2.7] - 2016-08-29
 ### Added
 - alternate codes for F1-F4 (fixes bpython #626)
--- curtsies/__init__.py
+++ curtsies/__init__.py
@@ -1,5 +1,5 @@
 """Terminal-formatted strings"""
-__version__='0.2.6'
+__version__='0.2.7'
 
 from .window import FullscreenWindow, CursorAwareWindow
 from .input import Input
