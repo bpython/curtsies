ec06b821d8a07d88bf201ab9adcb696624f02c55
Armira Nance <adnance99@gmail.com>
2020-07-02 07:44:53 -0500

    Added docstrings; changed speed of snake to make game more playable; prompts user to press enter before starting game

--- examples/demo_input_paste.py
+++ examples/demo_input_paste.py
@@ -10,10 +10,11 @@ def paste():
         print("If more than %d chars read in same read a paste event is generated" % input_generator.paste_threshold)
         for e in input_generator:
             print(repr(e))
-            time.sleep(1)
 
             if e == u'<ESC>':
                 break
+
+            time.sleep(1)
 
 if __name__ == '__main__':
     paste()
