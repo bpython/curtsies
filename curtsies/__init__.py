8d28e5bd39ad388d66558ca7f010fba4fb2ad174
Thomas Ballinger <thomasballinger@gmail.com>
2020-07-04 09:39:25 -0700

    whitespace change

--- curtsies/__init__.py
+++ curtsies/__init__.py
@@ -4,6 +4,6 @@ __version__='0.3.2'
 
 from .window import FullscreenWindow, CursorAwareWindow
 from .input import Input
-from .termhelpers import Nonblocking, Cbreak, Termmode 
+from .termhelpers import Nonblocking, Cbreak, Termmode
 from .formatstring import FmtStr, fmtstr
 from .formatstringarray import FSArray, fsarray
