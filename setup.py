e4c4090edafbc4b7730b1161b1b849dd8a0bb732
Thomas Ballinger <thomasballinger@gmail.com>
2020-07-15 13:57:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     name="curtsies",
     version=version(),
     description="Curses-like terminal wrapper, with colored strings!",
-    long_description=open("readme.md").read(),
+    long_description=open("readme.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/bpython/curtsies",
     author="Thomas Ballinger",
